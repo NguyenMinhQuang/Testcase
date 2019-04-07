6fe61460f5ab3a2e53117a09033eabe977dad452
NguyenMinhQuang <minhquang1230@gmail.com>
2019-04-07 23:09:11 +0700

    add

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dasboard</title>
+        <link rel="stylesheet" href="login.css" type="text/css" />
+    </head>
+    <body>
+        <div class="login-wrapper">
+            <div class="login-pane-layout"> 
+                <div class="login-title"><p> Wellcome Back! </p></div>
+                <form method="POST" action="#">
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Username" name="user"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Password" name="pwd"/>
+                    </div>
+                    <div class="form-group">
+                        <button class="login-button">Sign In</button>
+                    </div>
+                </form>
+        </div>
+    </body>
+</html>
